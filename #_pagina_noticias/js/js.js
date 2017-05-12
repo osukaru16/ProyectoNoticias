f213e1ea943f95c8eea444f57ed9ae746bb0ccff
osukaru16 <osukaru16@gmail.com>
2017-05-12 13:12:05 +0200

    Add files via upload

--- #_pagina_noticias/js/js.js
+++ #_pagina_noticias/js/js.js
@@ -1,1 +1,112 @@
-s
+
+
+//window.onload = function(){}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cargar json
+function cargaJ1(){
+
+$.getJSON( "json/json1.json", function( jsonObject ) {
+  //en aquest punt l'objecte jsonObject correspon al fitxer
+  pintar( jsonObject );
+});
+
+function pintar(json){
+ $.each( json, function( i, empleado ) {
+  $("#out").append( "<li id='" + i + "'>" + empleado.firstName+ " " + empleado.lastName + "</li>" );
+ });
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+function masCarga(){
+	alert("Miau");
+
+	$(#nuevoContenido).text("Miau");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+
+		var win = $(window);
+
+	// Each time the user scrolls
+		win.scroll(function() {
+		// End of the document reached?
+			if ($(document).height() - win.height() == win.scrollTop()) {
+				masCarga();
+			}
+		});
+	});
+
+
+
+
+
+/*
+
+	$(document).ready(function() {
+		var win = $(window);
+
+	// Each time the user scrolls
+		win.scroll(function() {
+		// End of the document reached?
+			if ($(document).height() - win.height() == win.scrollTop()) {
+				$('#loading').show();
+
+			$.ajax({
+				url: 'get-post.php',
+				dataType: 'html',
+				success: function(html) {
+					$('#posts').append(html);
+					$('#loading').hide();*
+				}
+			});
+			}
+		});
+	});
+*/
