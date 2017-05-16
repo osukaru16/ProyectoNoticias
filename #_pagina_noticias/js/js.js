4f26686292bcfbe3d3587f0caa8c429fb39fcb2a
osukaru16 <osukaru16@gmail.com>
2017-05-16 13:03:26 +0200

    Add files via upload

--- #_pagina_noticias/js/js.js
+++ #_pagina_noticias/js/js.js
@@ -9,7 +9,7 @@ window.onload = function(){
 
 
 
-	contador=4;
+	contador=1;
 	posicion=5;
 	desplegar=true;
 }
@@ -37,18 +37,38 @@ $.getJSON( "json/json1v2.json", function( jsonObject ) {
 
 function pintar(json){
 	$.each( json, function( key, val ) {
-$("#nuevoContenido").append("<div id='div_"+key+"' class='col-sm-5'> <div id='noticia3"+contador+"' class='col-sm-8' ><div class='thumbnail'>");
 
 
-  $("#nuevoContenido").append( "<p id='" + key+ "'>" + val.Titulo+"</p>" );
-  // posicion++;
+//$("#nuevoContenido").append("<div id='div_pos'"+posicion+" class='col-sm-5'></div>");
+$("#nuevoContenido").append("<div id='box_"+posicion+"' class='col-sm-5'></div>");
+$("#box_"+posicion).append("<div id='noticia"+posicion+"' class='col-sm-8'></div>");
+$("#noticia"+posicion).append("<div id='thumbnail"+posicion+"' class='thumbnail'></div>");
+$("#thumbnail"+posicion).append("<img src='"+val.Imagen+"' alt='noticia"+posicion+"' /> ");
+$("#thumbnail"+posicion).append("<p id='fecha"+posicion+ "'>" + val.Fecha+"</p>");
+$("#thumbnail"+posicion).append("<p id='titulo"+posicion+ "'> <strong>"+ val.Titulo+"</strong></p>");
+$("#thumbnail"+posicion).append("<p id='descripcion"+posicion+ "'>" + val.Descipcion+"</p>");
+$("#thumbnail"+posicion).append("<button class='btn' data-toggle='modal' data-target='#myModal' onclick='cargaNoticia("+posicion+");'>+</button>");
 
 
 
-$("#nuevoContenido").append("</div></div></div>");
+ 
+  posicion++;
 
 
-   if(key == 11){desplegar=false;}
+
+   if(key == 11){
+   	desplegar=false;
+
+$("#contenedor_boton_carga").css("display","none");
+
+
+$("#pie_principal").css("display","block");
+
+
+
+
+
+   }
 
  });
 
@@ -65,14 +85,14 @@ $("#nuevoContenido").append("</div></div></div>");
 /*
 function posicionar(numero){
 	if(numero%2 ==0){
-		$("#nuevoContenido").append("<div id='div_izquierda"+contador+"' class='col-sm-5'>");
+		$("#nuevoContenido").append("<div id='div_izquierda2' class='col-sm-5'>");
 	}
 	else{
 
 	}
 }
-*/
 
+*/
 
 
 
@@ -152,3 +172,50 @@ $(document).ready(function() {
 		});
 	});
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function cargaNoticia(posicion){
+
+alert("Cargar noticia "+posicion);
+
+
+/*
+$.post("noticias/noticias_extra.html",
+{
+          numero : posicion,
+          
+        }
+        )
+
+
+window.location.href = "noticias/noticias_extra.html";
+*/
+
+
+
+
+
+
+
+}
---
+++ #_pagina_noticias/js/js2.js
@@ -0,0 +1,63 @@
+var posicion=0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+function cargaNoticia(posicion){
+	//window.location.href = "noticias/noticias_extra.html";
+	alert("Cargar noticia "+posicion);
+
+	//$("#contenedor_extra").append("<h1>Noticias " +posicion+"</h1>");
+
+
+			
+       		$.post("noticias/noticias_extra.html", function(posicion){
+  			 $("#contenedor_extra").html(posicion);});
+
+
+
+}
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
