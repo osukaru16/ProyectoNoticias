0a3c6f90222081da8c7e6d0847a4eb342d1ee40d
osukaru16 <osukaru16@gmail.com>
2017-05-30 10:03:43 +0200

    Add files via upload

---
+++ pagina_noticias/js/js2.js
@@ -0,0 +1,35 @@
+/*var url = jQuery(location).attr('href');
+
+var titulo2 = document.title;*/
+
+//var title="IES Francesc de Borja Moll";
+
+var title;
+var url;
+
+
+
+window.onload = function(){
+	title = document.title;
+	//alert(titulo2);
+
+	url = jQuery(location).attr('href');
+
+	
+
+}
+
+
+//Nota no va pos javaScript
+
+/*
+$("#face").append("<a href='https://www.facebook.com/share.php?u="+url+"'><img src='../img/img_rss/iconoFace.jpg' /></a>");
+
+$("#twitter").append($("<a>",{href:"https://twitter.com/share?text="+title+"&url="+url,text:"enlace a twitter creado",rel:"nofollow"}));
+*/
+
+/*
+$("#face").append($("<a>",{href:"https://www.facebook.com/share.php?u="+url+"<img src='../img/img_rss/iconoFace.jpg' />",rel:"nofollow"}));
+
+$("#twitter").append($("<a>",{href:"https://twitter.com/share?text="+title+"&url="+url,text:"enlace a twitter creado",rel:"nofollow"}));
+*/
